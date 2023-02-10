1b3054735ecd89798da898db09cd0369c7948b1a
Alexandr Gulinsky <swith0711@gmail.com>
2023-02-10 09:14:14 +0200

    work with gallery,video,feedback

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`)
+
+const galleryElements = makeGalleryService(galleryItems)
+gallery.insertAdjacentHTML('afterbegin', galleryElements)
+
+function makeGalleryService(galleryItems) {
+    return galleryItems.map(galleryItem => {
+      return ` 
+          <a class="gallery__item" href="${galleryItem.original}">
+            <img 
+              class="gallery__image" 
+              src="${galleryItem.preview}" 
+              alt="${galleryItem.description}" 
+          </a>`
+    }).join('')
+  }
+
+ const lightbox = new SimpleLightbox('.gallery a', {
+   captionsData : 'alt',
+   captionDelay : 250
+ })
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle'
+import Player from '@vimeo/player'
 
+const iframe = document.querySelector(`iframe`)
+const player = new Player(iframe)
+
+const STORAGE_KEY = 'videoplayer-current-time'
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
+
+const onPlay = function(data) {
+  const videoUpdateTime = data.seconds
+ 
+  localStorage.setItem(STORAGE_KEY, videoUpdateTime)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function returnSaveTime() {
+  const saveTime = localStorage.getItem(STORAGE_KEY)
+
+  player.setCurrentTime(saveTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+  }).catch(function(error) {
+      switch (error.name) {
+        case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+  
+        default:
+        // some other error occurred
+        break;
+      }
+  });
+}
+
+returnSaveTime()
